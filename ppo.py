d71806124c4bf78d4aca855bef54c751e1216fe1
Stone Tejeda <sxtejeda98@gmail.com>
2019-11-29 14:36:16 -0600

    vim formatting makes me sad + agent does things

--- ppo.py
+++ ppo.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-import pomdp
+import pomdp as pd
 
 class ActorCritic(nn.Module):
     def __init__(self,  statespace_size, actionspace_size, hidden_size, learning_rate=1e-5, debug=True):
@@ -17,7 +17,7 @@ class ActorCritic(nn.Module):
         self.learning_rate = learning_rate
 
         self.actor_layer1 = nn.Linear(statespace_size, hidden_size )
-        self.actor_layer2 = nn.Linear(hidden_size, num_actions)
+        self.actor_layer2 = nn.Linear(hidden_size, actionspace_size)
 
         self.critic_layer1 = nn.Linear(statespace_size, hidden_size)
         self.critic_layer2 = nn.Linear(hidden_size, 1)
@@ -25,29 +25,29 @@ class ActorCritic(nn.Module):
 
     def action_distribution(self, state):
         action_distribution = F.relu(self.actor_layer1(state))
-        action_distribution = self.actor_layer2(state)
+        action_distribution = self.actor_layer2(action_distribution)
         if self.debug:
-            print("Action distribution: {}".format(action))
+            print("Action distribution: {}".format(action_distribution))
         return action_distribution
 
     def state_estimate(self, state):
-        state_estimate = F.relu(self.crtic_layer1(state))
-        state_estimate = self.critic_layer2(value)
+        state_estimate = F.relu(self.critic_layer1(state))
+        state_estimate = self.critic_layer2(state_estimate)
         if self.debug:
             print("State value estimate: {}".format(state_estimate))
         return state_estimate 
 
     def forward(self, state):
         if self.debug:
-            print("Input state: {}".forrmat(state))
+            print("Input state: {}".format(state))
 
         state_estimate = self.state_estimate(state)
         action_distribution = self.action_distribution(state)
 
-        return action_distribution, value
+        return action_distribution, state_estimate
 
 class PPO:
-    def __init__(self,  statespace_size, actionspace_size, hidden_size, device, learning_rate=1e-5, debug=True):
+    def __init__(self,  statespace_size, actionspace_size, hidden_size, device, learning_rate=1e-5, debug=True, gamma = 0.95):
 
         self.device = device
         self.actor_critic = ActorCritic(statespace_size, actionspace_size, hidden_size, learning_rate=1e-5).to(device)
@@ -57,7 +57,7 @@ class PPO:
 
         self.current_policy = None
         self.previous_policy = None
-
+        self.gamma = gamma
 
 
 
@@ -69,11 +69,15 @@ def __main__():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     hidden_size = 256
     env = gym.make('Ant-v2')
+    # Let's start simple and do the MDP before we get the rest of this 
+    pomdp = pd.PartiallyObservableEnv(env, seed=42)
+    num_iterations = 1
+
 
     #TODO Do we need this?
-    state = env.reset()
-    statespace_size = env.observation_space.shape[0]
-    actionspace_size = env.action_space.shape[0]
+    state = torch.tensor(pomdp.reset()).float().to(device)
+    statespace_size = pomdp.env.observation_space.shape[0]
+    actionspace_size = pomdp.env.action_space.shape[0]
 
     print("|S|: {}".format(statespace_size))
     print("|A|: {}".format(actionspace_size))
@@ -83,13 +87,29 @@ def __main__():
 
     agent = PPO(statespace_size, actionspace_size, hidden_size, device)
 
-    '''
+    
     for i in range(num_iterations):
-        state = env.reset()    
-        action_distribution, value = agent.actor_critic.forward(state)
+        memory_dict = {}
+
+        while True:
+            action_distribution, value = agent.actor_critic.forward(state)
+
+            action = F.softmax(action_distribution)
+            print(action)
+            action = action.detach().cpu()
+
+            next_state, reward, done, info = pomdp.step(action)
+            memory_dict['state'] = state
+            memory_dict['next_state'] = next_state
+            memory_dict['action_distro'] = action_distribution
+            memory_dict['reward'] = reward
 
 
+            if done:
+                break
+            print(action_distribution, value)
 
+    '''
 
     for i in range(episode_length):
         env.render()
